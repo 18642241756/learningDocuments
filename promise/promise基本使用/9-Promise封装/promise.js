0c565153ed40e64fe24cbbabd7697ddfb12a6499
18642241756 <3112869023@qq.com>
2023-12-05 16:12:10 +0800

    更新promise

--- promise/promise基本使用/9-Promise封装/promise.js
+++ promise/promise基本使用/9-Promise封装/promise.js
@@ -1,19 +1,57 @@
-function Promise(excutor){
+function Promise(excutor) {
 
-    function resolve(data){
+    this.promiseState = 'pending'
+    this.promiseResult = null
 
-    }
-
-    function reject(data){
-
-    }
-
-    // excutor 执行器函数 同步调用
-    excutor(resolve,reject)
+    // 保存 异步时没有立即执行的状态 
+    this.callback = {}
     
+    let self = this
+
+    function resolve(data) {
+        if (self.promiseState !== 'pending') return
+        // 1.修改对象 promiseState 状态
+        self.promiseState = 'fulfilled'
+        // 2.修改对象 promiseResult 结果值
+        self.promiseResult = data
+
+        if(self.callback.onResolve){
+            self.callback.onResolve(data)
+        }
+    }
+
+    function reject(data) {
+        if (self.promiseState !== 'pending') return
+        // 1.修改对象 promiseState 状态
+        self.promiseState = 'rejected'
+        // 2.修改对象 promiseResult 结果值
+        self.promiseResult = data
+
+        if(self.callback.onReject){
+            self.callback.onReject(data)
+        }
+    }
+    try {
+        // excutor 执行器函数 同步调用
+        excutor(resolve, reject)
+    } catch (error) {
+        reject(error)
+    }
+
 }
 
 //添加then方法
-Promise.prototype.then = function(onResolve,onReject){
-    
+Promise.prototype.then = function (onResolve, onReject) {
+    if (this.promiseState === 'fulfilled') {
+        onResolve(this.promiseResult)
+    }
+    if (this.promiseState === 'rejected') {
+        onReject(this.promiseResult)
+    }
+    if (this.promiseState === 'pending') {
+        this.callback = {
+            onResolve:onResolve,
+            onReject:onReject
+        }
+    }
 }
---
+++ promise/promise基本使用/9-Promise封装/promise.md
@@ -0,0 +1,19 @@
+封装promise步骤
+1.初始化结构搭建 √
+2.resolve和reject结构搭建 √
+3.resolve和reject代码实现 √
+4.throw抛出异常状态改变 √
+5.promise对象状态只能修改一次 √
+6.then方法执行回调 √
+7.异步任务回调执行 √
+8.指定多个回调的实现
+9.同步修改状态 then方法结果返回
+10.异步修改状态 then方法结果返回
+11.then方法完善与优化
+12.catch方法-异步穿透与传递值
+13.resolve方法封装
+14.reject 方法封装
+15.all方法封装
+16.race方法封装
+17.then方法回调的异步执行
+18.class版本的实现
